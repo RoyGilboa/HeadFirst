42510ab34a3998bf241916cdc8edb1f2613f14dd
Roy Gilboa <gilboa.roee@gmail.com>
2013-08-31 16:57:14 -0700

    first commit'

---
+++ Chap01/src/chap01/Bootstrap.java
@@ -0,0 +1,12 @@
+package chap01;
+
+public class Bootstrap {
+
+	public static void main(String[] args) {
+		
+		MyFirstApp myFirstApp = new MyFirstApp();
+		myFirstApp.roysMethod("Roy");
+
+	}
+
+}
---
+++ Chap01/src/chap01/MyFirstApp.java
@@ -0,0 +1,11 @@
+package chap01;
+
+
+public class MyFirstApp {
+	
+	public void roysMethod (String name) {
+		System.out.println("I ( " + name + " ) Rule!");
+		System.out.println("The World");
+	}
+	
+}
